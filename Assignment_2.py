d018661e45b5b3f7fdb94a981e4c845dad350605
Amisha453 <91615615+Amisha453@users.noreply.github.com>
2021-10-06 18:55:45 +0530

    commit

---
+++ Assignment_2.py
@@ -0,0 +1,42 @@
+#creating the list
+
+list_example=[]
+print(list_example)
+names=["Amisha","Ankita"]
+
+acc_ids=["AYC101","AYC105","AYC108"]
+
+gc_percentages=[40.4,76.8,88]
+
+print(names,acc_ids,gc_percentages)
+
+
+#nested lists
+records=[1,2,"Amisha","TAGAA",35.4,[1,2,3]]
+print(records)
+#creating the list
+
+list_example=[]
+print(list_example)
+names=["Amisha","Ankita"]
+
+acc_ids=["AYC101","AYC102","AYC103"]
+
+gc_percentages=[30.4,56.8,98]
+
+print(names,acc_ids,gc_percentages)
+
+
+#nested lists
+records=[1,2,"Amisha","TAGAA",35.4,[1,2,3]]
+print(records)
+#indexing and slicing
+#          -3       -2      -1
+acc_ids=["AYC101","AYC102","AYC103"]
+#          0        1        2
+print(acc_ids[0])
+print(acc_ids[2])
+print(acc_ids[-1])
+
+print(acc_ids[1:3])
+print(acc_ids[1:3][0])
