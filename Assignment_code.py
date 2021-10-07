f8815a010acfc96d47c3470fb0dad7c1c69ff1f8
Amisha453 <91615615+Amisha453@users.noreply.github.com>
2021-10-07 16:35:03 +0530

    commit

--- Assignment_code.py
+++ Assignment_code.py
@@ -2,7 +2,7 @@
 sequence=input("Please enter the sequence: ")
 print("The length of the sequnece=",len(sequence))
 
-#Write  a program to calculate the GC percentage of DNA sequence. Take the sequence from the user.
+#2.Write  a program to calculate the GC percentage of DNA sequence. Take the sequence from the user.
 seq = input("Please enter the Sequence:")
 total = len(seq)
 G = seq.count("G")
@@ -10,3 +10,9 @@ C = seq.count("C")
 GC_total = G+C
 GC_content = GC_total/float(total)
 print(GC_content)
+
+#3. program to mutate a sequence taken from user. Accept sequence as a input from the user. Take position and mutated base as argument.
+seq = input("Please enter the Sequence:")
+mutated_seq=seq[:3]+"A"+seq[4:]
+print(seq)
+print(mutated_seq)
